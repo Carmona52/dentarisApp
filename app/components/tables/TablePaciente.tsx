e62e59c8e4b42f17df11b50e8c6302c56c577eab
JesusCarmona <ryzen3000@outlook.es>
2025-07-13 20:00:12 -0600

    correción formulario de guardar paciente

---
+++ app/components/tables/TablePaciente.tsx
@@ -0,0 +1,20 @@
+import { DataGrid } from '@mui/x-data-grid';
+import React from "react";
+
+import {tablePacienteProps} from "@/app/miClinica/types/DataType";
+
+const TablaPacientes:React.FC<tablePacienteProps> = ({columns, data})=>{
+    return (
+        <DataGrid
+            rows={data}
+            columns={columns}
+            getRowId={(row) => row.numero_identificacion}
+            autoHeight
+            disableRowSelectionOnClick
+            sx={{ borderRadius: 2, boxShadow: 2 }}
+        />
+
+    );
+}
+
+export default TablaPacientes;
